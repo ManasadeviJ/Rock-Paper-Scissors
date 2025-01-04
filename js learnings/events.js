5c34cadfd483f7489a525ecbd066c8f01b401629
ManasadeviJ <manasadevij08.com>
2025-01-04 20:20:12 +0530

    Removed js learnings folder

--- js learnings/events.js
+++
@@ -1,8 +0,0 @@
-var btn1 = document.querySelector("#btn1");
-
-btn1.onclick = () => {
-    alert("BTN 1 clicked");
-    let b = 10;
-    b++;
-    alert(b);
-}
--- js learnings/pracstring.js
+++
@@ -1,4 +0,0 @@
-let fullName = prompt('Enter your fullname');
-let at= "@";
-let userName = at + fullName + fullName.length;
-console.log(userName);
